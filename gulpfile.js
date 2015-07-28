4a18f771d83ae856faa6e349eb7b3519c8693581
Steve M <smilovidov@gmail.com>
2015-07-28 14:03:25 -0400

    Set up gulpfile.js at per end of Unit1 Lesson2 part 2

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,59 @@ var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+
+gulp.task('jshint', function() {
+  return gulp.src('site/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('site/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('site/css'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('site/index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('scripts', function() {
+  return browserify('./site/js/main.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('site/css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('site/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('site/js/*.js', ['jshint']);
+  gulp.watch('site/scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['jshint', 'sass', 'watch']);
+
+gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
+
