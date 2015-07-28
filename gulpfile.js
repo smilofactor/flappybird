e545eeedf6b0b74055debd9f1db289ba5a48ed2f
Steve M <smilovidov@gmail.com>
2015-07-28 12:14:32 -0400

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
